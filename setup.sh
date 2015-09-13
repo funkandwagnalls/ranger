c18a09c1a1fe813acdbb65e345a255cb1a3dd029
chrisduffy <christopher.s.duffy@gmail.com>
2015-09-13 16:15:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,9 +53,8 @@ chmod a+x pv.ps1 im.ps1
 cp -p pv.ps1 im.ps1 /opt/ranger/smb/
 
 touch /opt/ranger/impacket && rm -rf /opt/ranger/impacket
-
-cd /opt/ranger && wget https://pypi.python.org/packages/source/i/impacket/impacket-0.9.13.tar.gz &&  tar -xzvf impacket-0.9.13.tar.gz
-rm  tar -xzvf impacket-0.9.13.tar.gz && mv mv impacket-0.9.13 impacket
-touch /usr/bin/ranger && rm /usr/bin/ranger
+touch /usr/bin/ranger && rm -f /usr/bin/ranger
+cd /opt/ranger && wget https://pypi.python.org/packages/source/i/impacket/impacket-0.9.13.tar.gz && tar -xzvf impacket-0.9.13.tar.gz
+rm -f tar -xzvf impacket-0.9.13.tar.gz && mv impacket-0.9.13 impacket
 cd /opt/ranger/impacket/examples && wget https://raw.githubusercontent.com/funkandwagnalls/ranger/master/ranger.py && chmod a+x ranger.py
 ln -s /opt/ranger/impacket/examples/ranger.py /usr/bin/ranger
